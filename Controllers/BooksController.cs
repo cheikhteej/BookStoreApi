bf4f8f85de57766638e7428569036f4465fd2489
unknown <lycheikh05@gmail.com>
2026-03-15 21:58:38 +0000

    controleur update

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -35,6 +35,14 @@ public class BooksController : ControllerBase
         return book;
     }
 
+    [HttpPost]
+    [Authorize(Roles = "Admin")] // Seul Admin peut ajouter
+    public async Task<IActionResult> Post(Book newBook)
+    {
+        await _booksService.CreateAsync(newBook);
+
+        return CreatedAtAction(nameof(Get), new { id = newBook.Id }, newBook);
+    }
 
     [HttpPut("{id:length(24)}")]
     [Authorize(Roles = "Admin")] // Seul Admin peut modifier
